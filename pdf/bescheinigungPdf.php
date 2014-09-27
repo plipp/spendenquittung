c7b6a691a03132066b868edf1aa685b70ba08e40
plipp <patricia.lipp@immobilienscout24.de>
2014-09-27 15:10:58 +0200

    new pdf generation functions

--- pdf/bescheinigungPdf.php
+++ pdf/bescheinigungPdf.php
@@ -48,9 +48,20 @@ class BescheinigungPDF extends FPDF
             $this->_addressData['zip']." ".$this->_addressData['city'] ."\n",
             1);
 
+        $this->topicBox("Tag der Zuwendung, Wert der Zuwendung:", date("d.m.Y"). ", TODO\n");
         $this->topicBox("Genaue Bezeichnung der Sachzuwendung mit Alter, Zustand, Kaufpreis usw.:","\ns. Anhang 'Spendenliste'\n");
 
-        // todo http://fpdf.de/downloads/addons/56/ ...
+        $this->bulletText("Zutreffendes Bitte Ankreuzen", array("Die Sachzuwendung stammt nach den Angaben des Zuwendenden aus dem Betriebsvermögen. ".
+            "Die Zuwendung wurde nach dem Wert der Entnahme (ggf. mit dem niedrigeren gemeinen Wert) und nach der Umsatzsteuer, ".
+            "die auf die Entnahme entfällt, bewertet.",
+            "Die Sachzuwendung stammt nach den Angaben des Zuwendenden aus dem Privatvermögen.",
+            "Der Zuwendende hat trotz Aufforderung keine Angaben zur Herkunft der Sachzuwendung gemacht.",
+            "Geeignete Unterlagen, die zur Wertermittlung gedient haben, z. B. Rechnung, Gutachten, liegen vor."
+        ));
+        $this->topicBox("",
+            "Es wird bestätigt, dass die Zuwendung nur zur Förderung des Berliner Büchertisches verwendet wird.
+            \n\n\n\n Ort, Datum, Unterschrift des Zuwendungsempfängers");
+
     }
 
     private function topicBox($topic, $content)
@@ -73,6 +84,77 @@ class BescheinigungPDF extends FPDF
         $this->SetFont('Arial', '', 8);
         $this->MultiCell(self::BOX_SIZE, 4, utf8_decode($infoText));
         $this->Ln();
+    }
+
+    private function bulletText ($title, $text_array) {
+        $column_width = $this->w-30;
+
+        $bulletList = array();
+        $bulletList['bullet'] = 'o';
+        $bulletList['margin'] = ' ';
+        $bulletList['indent'] = 2;
+        $bulletList['spacer'] = 2;
+        $bulletList['text'] = array_map("utf8_decode",$text_array);
+        $this->SetX(20);
+        $this->MultiCellBltArray($column_width-$this->x, 6, $bulletList);
+        $this->Ln(10);
+
+    }
+    /************************************************************
+     *                                                           *
+     *    MultiCell with bullet (array)                          *
+     *                                                           *
+     *    Requires an array with the following  keys:            *
+     *                                                           *
+     *        Bullet -> String or Number                         *
+     *        Margin -> Number, space between bullet and text    *
+     *        Indent -> Number, width from current x position    *
+     *        Spacer -> Number, calls Cell(x), spacer=x          *
+     *        Text -> Array, items to be bulleted                *
+     *                                                           *
+     *    see http://fpdf.de/downloads/addons/56/
+     ************************************************************/
+
+    private function MultiCellBltArray($w, $h, $blt_array, $border=0, $align='J', $fill=0)
+    {
+        if (!is_array($blt_array))
+        {
+            die('MultiCellBltArray requires an array with the following keys: bullet, margin, text, indent, spacer');
+            exit;
+        }
+
+        //Save x
+        $bak_x = $this->x;
+
+        for ($i=0; $i<sizeof($blt_array['text']); $i++)
+        {
+            //Get bullet width including margin
+            $blt_width = $this->GetStringWidth($blt_array['bullet'] . $blt_array['margin'])+$this->cMargin*2;
+
+            // SetX
+            $this->SetX($bak_x);
+
+            //Output indent
+            if ($blt_array['indent'] > 0)
+                $this->Cell($blt_array['indent']);
+
+            //Output bullet
+            $this->Cell($blt_width, $h, $blt_array['bullet'] . $blt_array['margin'], 0, '', $fill);
+
+            //Output text
+            $this->MultiCell($w-$blt_width, $h, $blt_array['text'][$i], $border, $align, $fill);
+
+            //Insert a spacer between items if not the last item
+            if ($i != sizeof($blt_array['text'])-1)
+                $this->Ln($blt_array['spacer']);
+
+            //Increment bullet if it's a number
+            if (is_numeric($blt_array['bullet']))
+                $blt_array['bullet']++;
+        }
+
+        //Restore x
+        $this->x = $bak_x;
     }
 
     private function initialPdfSettings()
